507419319f2dfdde96c38cd4975055a606fa7b9e
hootie42502 <131285920+hootie42502@users.noreply.github.com>
2023-05-01 10:13:08 -0500

    Update app.module.ts

--- Docker/frontend/src/app/app.module.ts
+++ Docker/frontend/src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { StoreComponent } from './store/store.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CartComponent } from './cart/cart.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    StoreComponent,
+    CartComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
