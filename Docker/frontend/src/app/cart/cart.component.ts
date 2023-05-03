81195d4f449abf9740f61d21939916bc135d2065
Eric Hooten <ebhgcp@umsystem.edu>
2023-05-03 12:29:27 -0500

    changeDitectionStrategy

--- Docker/frontend/src/app/cart/cart.component.ts
+++ Docker/frontend/src/app/cart/cart.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,ChangeDetectionStrategy } from '@angular/core';
 import { Item } from 'src/app/interfaces/item';
 import { StoreService } from '../store.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CartComponent {
   items: Item[] = [];
