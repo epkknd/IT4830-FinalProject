9f13f363fbe19bd215cf1cfeb5a5d61d8a2bb7e1
Eric Hooten <ebhgcp@umsystem.edu>
2023-05-03 12:24:15 -0500

    cart-component

--- Docker/frontend/src/app/cart/cart.component.ts
+++ Docker/frontend/src/app/cart/cart.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Item } from 'src/app/interfaces/item';
+import { StoreService } from '../store.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+  items: Item[] = [];
 
+  constructor(private storeService: StoreService) {
+  }
+
+  ngOnInit() {
+    this.items = this.storeService.getCartItems();
+  }
+
+  readItems() {
+    console.log(this.storeService.getCartItems());
+  }
+
+  removeItem(item: Item) {
+    this.storeService.removeItem(item);
+  }
 }
